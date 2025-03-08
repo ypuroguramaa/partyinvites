1f58716085da00baf8147b4326200886f78ac80b
ypuroguramaa <188652197+ypuroguramaa@users.noreply.github.com>
2025-03-08 20:40:50 +0800

    add new line of code in main.go file and add 3 new html files

--- main.go
+++ main.go
@@ -2,6 +2,13 @@ package main
 
 import "fmt"
 
+type Rsvp struct {
+	Name, Email, Phone string
+	WillAttend         bool
+}
+
+var responses = make([]*Rsvp, 0, 10)
+
 func main() {
 	fmt.Println("TODO: add some features")
 }
