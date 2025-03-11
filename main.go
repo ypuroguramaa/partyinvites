04a42fed9af4c4ae335b91c0a95ca493624c8492
ypuroguramaa <188652197+ypuroguramaa@users.noreply.github.com>
2025-03-11 15:56:15 +0800

    add more lines of code in main.go file, to fill the missing piece to the functionality of go project on handling details in form and submitting them

--- main.go
+++ main.go
@@ -32,7 +32,7 @@ func welcomeHandler(writer http.ResponseWriter, request *http.Request) {
 }
 
 func listHandler(writer http.ResponseWriter, request *http.Request) {
-	templates["list"].Execute(writer, nil)
+	templates["list"].Execute(writer, responses)
 
 }
 
@@ -44,7 +44,40 @@ type formData struct {
 func formHandler(writer http.ResponseWriter, request *http.Request) {
 	if request.Method == http.MethodGet {
 		templates["form"].Execute(writer, formData{Rsvp: &Rsvp{}, Errors: []string{}})
+	} else if request.Method == http.MethodPost {
+		request.ParseForm()
+		responseData := Rsvp{
+			Name:       request.Form["name"][0],
+			Email:      request.Form["email"][0],
+			Phone:      request.Form["phone"][0],
+			WillAttend: request.Form["willattend"][0] == "true",
+		}
+
+		errors := []string{}
+
+		if responseData.Name == "" {
+			errors = append(errors, "Please Enter Your Name")
+		}
+		if responseData.Email == "" {
+			errors = append(errors, "Please enter your email address")
+		}
+		if responseData.Phone == "" {
+			errors = append(errors, "Please enter your phone number")
+		}
+		if len(errors) > 0 {
+			templates["form"].Execute(writer, formData{Rsvp: &responseData, Errors: errors})
+		} else {
+
+			responses = append(responses, &responseData)
+
+			if responseData.WillAttend {
+				templates["thanks"].Execute(writer, responseData.Name)
+			} else {
+				templates["sorry"].Execute(writer, responseData.Name)
+			}
+		}
 	}
+
 }
 
 func main() {
