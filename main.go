fc553a5c867e22a137333b1b3e6defdc4db03105
ypuroguramaa <188652197+ypuroguramaa@users.noreply.github.com>
2025-03-11 11:18:54 +0800

    Added 3 new html files and new lines of code on main.go file

--- main.go
+++ main.go
@@ -1,6 +1,10 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"html/template"
+	"net/http"
+)
 
 type Rsvp struct {
 	Name, Email, Phone string
@@ -8,7 +12,40 @@ type Rsvp struct {
 }
 
 var responses = make([]*Rsvp, 0, 10)
+var templates = make(map[string]*template.Template, 3)
+
+func loadTemplates() {
+	templateNames := [5]string{"welcome", "form", "thanks", "sorry", "list"}
+	for index, name := range templateNames {
+		t, err := template.ParseFiles("layout.html", name+".html")
+		if err == nil {
+			templates[name] = t
+			fmt.Println("Loaded template", index, name)
+		} else {
+			panic(err)
+		}
+	}
+}
+
+func welcomeHandler(writer http.ResponseWriter, request *http.Request) {
+	templates["welcome"].Execute(writer, nil)
+}
+
+func listHandler(writer http.ResponseWriter, request *http.Request) {
+	templates["list"].Execute(writer, nil)
+
+}
 
 func main() {
-	fmt.Println("TODO: add some features")
+
+	loadTemplates()
+
+	http.HandleFunc("/", welcomeHandler)
+
+	http.HandleFunc("/list", listHandler)
+
+	err := http.ListenAndServe(":5000", nil)
+	if err != nil {
+		fmt.Println(err)
+	}
 }
